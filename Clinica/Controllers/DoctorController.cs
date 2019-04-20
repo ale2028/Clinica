2463c2d2cfcd1d1406101302605f2d9d6cb1f8d2
Ale <ale.alvarez97@hotmail.com>
2019-04-20 02:24:37 -0600

    update

---
+++ Clinica/Controllers/DoctorController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Clinica.Models;
+
+namespace Clinica.Controllers
+{
+    public class DoctorController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            WsClinica.WS_SERVER server = new WsClinica.WS_SERVER();
+            var result = server.;
+            List<WsClinica.Entity_User> users = new List<WsClinica.Entity_User>();
+            return View(users);
+        }
+
+        /// <summary>
+        /// M<uestra la vista
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Recibe los datos de la vista
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>s
+        [HttpPost]
+        public ActionResult Register(Doctor model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Llamamos al servicio web para registrar un nuevo doctor
+                WsClinica.WS_SERVER server = new WsClinica.WS_SERVER();
+                string result = server.Registrar_Doctor(model.AsWebServiceModel());
+                Console.WriteLine(result);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("Faltan datos", "No todos los datos requeridos, están presentes");
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Clinica/Models/Doctor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Clinica.Models
+{
+    public class Doctor
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Cedula { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Nombre { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Apellido1 { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Apellido2 { set; get; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Correo { set; get; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { set; get; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Especialidad { set; get; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Telefono { set; get; }
+
+        public WsClinica.Entity_Doctor AsWebServiceModel()
+        {
+            WsClinica.Entity_Doctor doctor = new WsClinica.Entity_Doctor();
+            doctor.apellido1 = this.Apellido1;
+            doctor.apellido2 = this.Apellido2;
+            doctor.cedula = this.Cedula;
+            doctor.correo = this.Correo;
+            doctor.especialidad = this.Especialidad;
+            doctor.nombre = this.Nombre;
+            doctor.password = this.Password;
+            doctor.telefono = this.Telefono;
+            return doctor;
+        }
+
+    }
+}
